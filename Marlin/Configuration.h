bc3994030fd344fb258499f96b513f505590a950
Frank Claycomb <frank@splra.org>
2020-06-14 13:55:50 -0400

    disabled bootscreen. added NOZZLE_PARK_FEATURE and ADVANCED_PAUSE_FEATURE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -86,7 +86,7 @@
  */
 
 // Show the Marlin bootscreen on startup. ** ENABLE FOR PRODUCTION **
-#define SHOW_BOOTSCREEN
+//#define SHOW_BOOTSCREEN
 
 // Show the bitmap in Marlin/_Bootscreen.h on startup.
 #define SHOW_CUSTOM_BOOTSCREEN
@@ -1497,7 +1497,7 @@
  *    P1  Raise the nozzle always to Z-park height.
  *    P2  Raise the nozzle by Z-park amount, limited to Z_MAX_POS.
  */
-//#define NOZZLE_PARK_FEATURE
+#define NOZZLE_PARK_FEATURE
 
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z_raise }
